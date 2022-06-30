11a0f02d51fe99fe33a30f4f02aa9f1c2ec9db3b
Omar Aljazairy <omar@fedal.nl>
2022-06-30 19:05:11 +0200

    added the mirbad radio

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Radio = (props) => (
+  <>
   <div>
-    <h3>Radio Alrasheed</h3>
+    <h3>Radio Alrasheed2</h3>
     <audio controls="controls" src="http://alrasheedmedia.com:9998/stream"></audio>
-  </div>
+  <h3>Radio Almarbad</h3>
+  <audio controls="controls" src="https://listen.mixlr.com/45ab5444b043a4488d8967622ee958be"></audio>
+  </div>  
+  </>
   )
   
 
